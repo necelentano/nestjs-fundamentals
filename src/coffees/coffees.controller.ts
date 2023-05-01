d7b3c5ed0a4abc45eedf71a2b0a1dc3eccc8c58f
Necelentano <necelentano@gmail.com>
2023-05-01 15:15:11 +0300

    Implementing Pagination with Query Parameters

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -8,14 +8,17 @@ import {
   Param,
   Patch,
   Post,
+  Query,
   Res,
 } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
-  findAll() {
-    return 'This action returns all coffees!';
+  findAll(@Query() paginationQuery) {
+    // /coffees?limit=20&offset=10
+    const { limit, offset } = paginationQuery;
+    return `This action returns all coffees! Limit: ${limit}, offset: ${offset}`;
   }
 
   // Response Status Code
