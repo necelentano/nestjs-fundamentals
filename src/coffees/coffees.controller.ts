1caa9f9575e08030c0669eb2890d74b1259adaa4
Necelentano <necelentano@gmail.com>
2023-05-01 14:42:52 +0300

    Use route parameters & Handling request body payload

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get('flavors')
   findAll() {
     return 'This action returns all coffees!';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} coffee!`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
   }
 }
