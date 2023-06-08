155072319fb7559720dd235dec4e369e0d642d46
Necelentano <necelentano@gmail.com>
2023-06-08 16:04:35 +0300

    Request Logging with Middleware

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,11 +1,25 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { ApiKeyGuard } from './guards/api-key/api-key.guard';
 import { ConfigModule } from '@nestjs/config';
+import { LoggingMiddleware } from './middleware/logging/logging.middleware';
 
 // we need this module because we use dependency injection in our guard
 @Module({
   imports: [ConfigModule],
   providers: [{ provide: APP_GUARD, useClass: ApiKeyGuard }],
 })
-export class CommonModule {}
+export class CommonModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggingMiddleware)
+      //.exclude('coffees').forRoutes('*');
+      //.forRoutes({ path: 'coffees', method: RequestMethod.GET });
+      .forRoutes('*');
+  }
+}
